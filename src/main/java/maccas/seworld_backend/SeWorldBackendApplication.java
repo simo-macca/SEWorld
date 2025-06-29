fb26429afa0165060e94f8ff3a79efb468809eea
Simone <simone.maccario04@gmail.com>
2025-06-29 22:45:50 +0200

    initial commit

---
+++ src/main/java/maccas/seworld_backend/SeWorldBackendApplication.java
@@ -0,0 +1,13 @@
+package maccas.seworld_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeWorldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeWorldBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maccas/seworld_backend/SeWorldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package maccas.seworld_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeWorldBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
