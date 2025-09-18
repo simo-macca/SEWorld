08a101d9dcf9dd8b03b51d439d732fd3e1f4de6b
Simone <simone.maccario04@gmail.com>
2025-09-18 12:38:33 +0200

    fix variable name problem in exercise's page

--- src/components/ExerciseComponent.vue
+++ src/components/ExerciseComponent.vue
@@ -10,7 +10,7 @@ import { useToast } from 'vue-toastification'
 
 // Props definition
 const props = defineProps({
-  material: {
+  exercise: {
     type: Object,
     required: true,
   },
@@ -54,7 +54,7 @@ async function confirmDelete(slug) {
     <b-card class="h-100">
       <!-- Header -->
       <div class="d-flex justify-content-between align-items-center">
-        <h5>{{ material['exerciseTitle'] }}</h5>
+        <h5>{{ exercise['exerciseTitle'] }}</h5>
         <div class="d-flex align-items-center gap-2" v-if="!isInstructor">
           <component
             :is="getStatusConfig(props.material['exerciseIsCompleted']).iconComponent"
@@ -68,16 +68,16 @@ async function confirmDelete(slug) {
       </div>
 
       <!-- Description -->
-      <p>{{ material['exerciseDescription'] }}</p>
+      <p>{{ exercise['exerciseDescription'] }}</p>
 
       <!-- Footer -->
       <div class="d-flex justify-content-between align-items-center pt-3">
         <small class="d-flex flex-column row-gap-1">
           <span v-if="exercise['exerciseCreatedDate']" class="text-secondary-light"
-            >Created on: {{ material['exerciseCreatedDate'] }}</span
+            >Created on: {{ exercise['exerciseCreatedDate'] }}</span
           >
           <span v-if="exercise['exerciseOwner']" class="text-secondary-light"
-            >by: {{ material['exerciseOwner'] }}</span
+            >by: {{ exercise['exerciseOwner'] }}</span
           >
         </small>
 
